5fb53d1a3403d032ee9f6f954f3982cd355c4c39
Moises Edson Yañez Villa <yanezvilla.edson@gmail.com>
2022-08-12 21:09:03 -0400

    Se esta agregando expresion regular para poder hallar las coicidencias

--- script.js
+++ script.js
@@ -9,12 +9,18 @@ const dataToEnpcrypt = {
 }
 
 const encrypt = () => {
+    expresion = /[aeiou]/g
     const inputEncrypt = document.querySelector("[input-encrypter]");
     let message = inputEncrypt.value;
-    Object.keys(dataToEnpcrypt).forEach( key => {
-        message = message.replace(key, dataToEnpcrypt[key])
-    });
-    console.log(message);
+    // Object.keys(dataToEnpcrypt).forEach( key => {
+    //     message = message.replace(key, dataToEnpcrypt[key])
+    // });
+    // console.log(message);
+    while ((result= expresion.exec(message))) {
+        console.log(dataToEnpcrypt[result[0]]);
+        console.log(result);
+
+    }
 }
 
 button_encrypt.addEventListener("click", encrypt);
